ad2f0cfa9c5f4ed42eb2993d3d5353b9a293d303
Javonte Tuppince <taetupp47@gmail.com>
2021-01-31 14:39:35 -0500

    made a seed file to populate databse

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('bacon');
+INSERT INTO burgers (burger_name) VALUES ('burger with cheese');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Wooper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bigmak', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smudge', true);
+INSERT INTO burgers (burger_name) VALUES ('Daisy');

1ec55ad658e36e20ea21050586e0419ad8222043
Javonte Tuppince <taetupp47@gmail.com>
2021-02-04 05:18:50 -0500

    debugging

--- models/burger.js
+++ models/burger.js
@@ -8,8 +8,8 @@ var burger = {
       });
     },
     // The variables cols and vals are arrays.
-    create: function(cols, vals, cb) {
-      orm.create("burger", cols, vals, function(res) {
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burger", cols, vals, function(res) {
         cb(res);
       });
     },

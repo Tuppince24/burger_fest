bbad05912bfbe1093d93f2bd3423d37cbe623288
Javonte Tuppince <taetupp47@gmail.com>
2021-02-05 18:07:44 -0500

    controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,62 +1,45 @@
 var express = require("express");
+
 var router = express.Router();
 
-var burger = require("../models/burger.js");
+var burger = require("../models/burger");
 
-
-
-//get method
-router.get("/", function(req, res){
-    burger.all(function(data){
-        var hbsObject = {
-            burger: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
     });
-});
+  });
 
-//post method 
-router.post("/api/burger", function(req, res){
+router.post("/api/burgers", function(req, res) {
+    console.log(req.body)
     burger.insertOne([
-        "burger_name", "devoured"
-    ],[
+        "burger_name"
+    ], [
         req.body.burger_name
     ], function(result) {
-        res.json({ id: result.insertID });
+        
+        res.json({ id: result.insertId });
     });
 });
 
-// put method 
-router.put("/api/burger/:id", function(req, res){
-    var condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
-    console.log("condition", condition);
+  console.log("condition", condition);
 
-    burger.updateOne({
-        devoured: req.body.devoured
-    }, condition, function(result){
-        if (result.changeRows === 0){
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-
-    // router.delete("/api/burger/:id", function(req, res){
-    //     var condition = "id = " + req.params.id;
-
-    //     burger.delete(condition, function(result){
-    //         if (result.affectedRows == 0) {
-    //             return res.status(404).end();
-    //         } else{
-    //             res.status(200).end();
-    //         }
-    //     });
-    // });
-
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
 });
-
-console.log(burger)
 
 module.exports = router;
